7b1669fa70226fd075dc051fb319fa6b10fef752
hmarui66 <hmarui66@gmail.com>
2022-06-24 00:33:39 +0900

    impl file mgr

---
+++ src/file/FileTest.java
@@ -0,0 +1,25 @@
+package file;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileTest {
+    public static void main(String[] args) {
+        System.out.println();
+        var dir = new File("dbdir");
+        var fm = new FileMgr(dir, 500);
+        var blk = new BlockId("testfile", 2);
+        var p1 = new Page(fm.blocksize());
+        int pos1 = 88;
+        p1.setString(pos1, "abcdefghijklm");
+        var size = Page.maxLength("abcdefghijklm".length());
+        var pos2 = pos1 + size;
+        p1.setInt(pos2, 345);
+        fm.write(blk, p1);
+
+        var p2 = new Page(fm.blocksize());
+        fm.read(blk, p2);
+        System.out.println("offset " + pos2 + " contains " + p2.getInt(pos2));
+        System.out.println("offset " + pos1 + " contains " + p2.getString(pos1));
+    }
+}
