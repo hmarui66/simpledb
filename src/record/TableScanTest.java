7eeac49b710bafe6c02d236b278aa84eede0725f
hmarui66 <hmarui66@gmail.com>
2022-08-20 16:42:20 +0900

    impl TableMgr

--- src/record/TableScanTest.java
+++ src/record/TableScanTest.java
@@ -3,30 +3,32 @@ package record;
 import server.SimpleDB;
 import tx.Transaction;
 
+@SuppressWarnings("DuplicatedCode")
 public class TableScanTest {
     public static void main(String[] args) {
         SimpleDB db = new SimpleDB("dbdir/tabletest", 400, 8);
+        // トランザクション開始
         Transaction tx = db.newTx();
-        Schema sch = new Schema();
-        sch.addIntField("A");
-        sch.addStringField("B", 9);
-        Layout layout = new Layout(sch);
-        for (String fldname : layout.schema().fields()) {
-            int offset = layout.offset(fldname);
-            System.out.println(fldname + " has offset " + offset);
-        }
 
+        // スキーマ定義(A int, B varchar(9))
+        Schema schema = new Schema();
+        schema.addIntField("A");
+        schema.addStringField("B", 9);
+        Layout layout = new Layout(schema);
+
+        // テーブルスキャンクラス(読み込みだけでなく更新操作もハンドリング)
         TableScan ts = new TableScan(tx, "T", layout);
-        System.out.println("Filling the table with 50 random records.");
+
+        // インサート操作
         ts.beforeFirst();
         for (int i = 0; i < 50; i++) {
-            ts.insert();
+            ts.insert(); // レコード挿入先の slot へ移動
             int n = (int) Math.round(Math.random() * 50);
             ts.setInt("A", n);
             ts.setString("B", "rec"+n);
-            System.out.println("inserting into slot " + ts.getRid() + ": {" + n + ", rec" + n + "}");
         }
-        System.out.println("Deleting records with A-value < 25.");
+
+        // 削除操作
         int count = 0;
         ts.beforeFirst();
         while (ts.next()) {
@@ -38,9 +40,8 @@ public class TableScanTest {
                 ts.delete();
             }
         }
-        System.out.println(count + " values under 25 were deleted.\n");
 
-        System.out.println("Here are the remaining records");
+        // 読み込み操作
         ts.beforeFirst();
         while (ts.next()) {
             int a = ts.getInt("A");
